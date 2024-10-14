49e5e5ce771165c36ff2358628d61e69cd84f7f8
Karan60 <nathkaran327@gmail.com>
2024-10-14 08:03:36 +0530

    api

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -21,14 +21,19 @@ axiosInstance.interceptors.request.use(
 
 axiosInstance.interceptors.response.use(
     function (response) {
+        //step global loader here
         return processResponse(response);
     },
     function(error) {
+        //step global loader here
         return Promise.reject(processError(error));
     }
 )
 
-
+//////////////
+//if success -> return {isSucess: true, data: object }
+//if fail -> return {isFailure: true, status: string, msg:string, code:int }
+/////////////
 const processResponse = (response) => {
     if(response?.status === 200) {
         return { isSucess: true, data: response.data}
@@ -39,6 +44,17 @@ const processResponse = (response) => {
             msg: response?.msg,
             code: response?.code
         }
+    }
+}
+
+const processError =(error)=>{
+    if(error.response){
+        //request made and server rerver responded with a status oyher
+        //that falls out of the range 2.x.x
+    }else if (error.request){
+        //request made but no response was receive
+    }else{
+        //something happend in setting up request that triggers an error
     }
 }
 
